f580e2d52a709999b9c01124ac7af5e80ccaec47
Adarsh Chavhan <adarshchavhan2@gmail.com>
2023-02-25 15:10:42 +0530

    WQ

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server:{
     port: 5500,
     proxy: {
-      '/api/v1': 'https://sample-seven-nu.vercel.app'
+      '/api/v1': 'https://sample-blogs.vercel.app'
     }
   },
   plugins: [react()],
